a2a9e23b7fd104039b780e4f7f0338e9f4ef09dd
skmajumder <skmajumder.cse@gmail.com>
2023-06-25 14:13:10 +0600

    .htaccess add

--- includes/nav.php
+++ includes/nav.php
@@ -8,10 +8,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">Register</a>
                 </li>
             </ul>
         </div>
---
+++ login.php
@@ -0,0 +1,22 @@
+<!-- Header -->
+<?php include('./includes/header.php'); ?>
+
+<!-- Nav menu -->
+<?php include('./includes/nav.php'); ?>
+
+<section class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="mt-4 p-5 bg-light rounded">
+                    <h1>Login page</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Footer -->
+<?php include('./includes/footer.php'); ?>
+
+

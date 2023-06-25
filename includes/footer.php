403b372b21a9fefbefe769b1672922ec8e259229
skmajumder <skmajumder.cse@gmail.com>
2023-06-25 13:52:41 +0600

    PHP file structure

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,26 +1,22 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- Google Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
-          rel="stylesheet">
-    <!-- Title -->
-    <title>Assignment Task</title>
-    <!-- Style -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
+<!-- Header -->
+<?php include('./includes/header.php'); ?>
 
-<!-- Scripts -->
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/script.js"></script>
-</body>
-</html>
+<!-- Nav menu -->
+<?php include('./includes/nav.php'); ?>
+
+<section class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="mt-4 p-5 bg-light rounded">
+                    <h1>Homepage</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Footer -->
+<?php include('./includes/footer.php'); ?>
+
+

88a903dc16f3443138a2997fa595f0557ed7234b
skmajumder <skmajumder.cse@gmail.com>
2023-06-25 14:40:10 +0600

    DB connection

--- functions/db.php
+++ functions/db.php
@@ -1,1 +1,11 @@
 <?php
+
+// Database configuration
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "juhosi-task";
+
+// Create a new MySQLi connection
+$conn = new mysqli($servername, $username, $password, $database);
+
--- functions/init.php
+++ functions/init.php
@@ -2,3 +2,8 @@
 
 include('db.php');
 include('functions.php');
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}

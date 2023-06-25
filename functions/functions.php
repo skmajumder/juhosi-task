a0efcc0f2e8bb2020be3c47e4912ef1912820c1a
skmajumder <skmajumder.cse@gmail.com>
2023-06-25 22:01:39 +0600

    Register form validate

--- functions/functions.php
+++ functions/functions.php
@@ -1,1 +1,112 @@
 <?php
+
+function validation_errors($error_message)
+{
+    $error_message = <<<DELIMITER
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Holy guacamole!</strong>
+            $error_message
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+DELIMITER;
+    return $error_message;
+}
+
+function clean($string)
+{
+    return htmlentities($string);
+}
+
+function redirect($location)
+{
+    return header("Location: {$location}");
+}
+
+function set_message($message)
+{
+    if (!empty($message))
+        $_SESSION['message'] = $message;
+    else
+        $_SESSION = "";
+}
+
+function display_message()
+{
+    if (isset($_SESSION['message'])) {
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
+    }
+}
+
+function token_generator()
+{
+    $_SESSION['token'] = md5(uniqid(mt_rand(), true));
+    return $token = $_SESSION['token'];
+}
+
+function email_exists($email)
+{
+    $sql = "SELECT id FROM users WHERE email = '$email'";
+    $result = query($sql);
+    confirm($result);
+    if (row_count($result) == 1) return true;
+    else return false;
+}
+
+function username_exists($user_name)
+{
+    $sql = "SELECT id FROM users WHERE username = '$user_name'";
+    $result = query($sql);
+    confirm($result);
+    if (row_count($result) == 1) return true;
+    else return false;
+}
+
+/** Validate register user */
+
+function validate_user_registration()
+{
+    $errors = [];
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $user_name = clean($_POST['username']);
+        $email = clean($_POST['email']);
+        $password = clean($_POST['password']);
+        $confirm_password = clean($_POST['confirm-password']);
+
+        if (username_exists($user_name)) {
+            $errors[] = "<span class='server_error_message'> ID(UserName) taken, Try another</span>";
+        }
+
+        if (email_exists($email)) {
+            $errors[] = "<span class='server_error_message'>This email {$email} already exits, Try another.</span>";
+        }
+
+        if (empty($user_name)) {
+            $errors[] = "<span class='server_error_message'>ID (Username) can't be empty</span>";
+        }
+
+        if (empty($email)) {
+            $errors[] = "<span class='server_error_message'>email can't be empty</span>";
+        }
+
+        if (empty($password)) {
+            $errors[] = "<span class='server_error_message'>Password Can't Empty</span>";
+        }
+
+        if (empty($confirm_password)) {
+            $errors[] = "<span class='server_error_message'>input confirm password</span>";
+        }
+
+        if ($confirm_password !== $password) {
+            $errors[] = "<span class='server_error_message'>Passwords do not match.</span>";
+        }
+
+        if (!empty($errors)) {
+            foreach ($errors as $error) {
+                echo validation_errors($error);
+                return;
+            }
+        }
+    }
+}
--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <!-- Header -->
-<?php include('./includes/header.php'); ?>
+<?php include('includes/header.php'); ?>
 
 <!-- Nav menu -->
-<?php include('./includes/nav.php'); ?>
+<?php include('includes/nav.php'); ?>
 
 <section class="section">
     <div class="container">
@@ -16,7 +16,49 @@
     </div>
 </section>
 
+<section class="section register-info">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3 py-5 text-center">
+                <?php validate_user_registration(); ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-register">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3 py-5">
+                <form id="registrationForm" method="post" onsubmit="return validateForm()">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">ID (Username):</label>
+                        <input type="text" class="form-control" id="username" name="username" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password:</label>
+                        <input type="password" class="form-control" minlength="6" id="password" name="password"
+                               required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="confirm-password" class="form-label">Confirm Password:</label>
+                        <input type="password" class="form-control" minlength="6" id="confirm-password"
+                               name="confirm-password"
+                               required>
+                    </div>
+                    <button type="submit" name="register-submit" class="btn btn-primary">Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 <!-- Footer -->
-<?php include('./includes/footer.php'); ?>
+<?php include('includes/footer.php'); ?>
 
 
